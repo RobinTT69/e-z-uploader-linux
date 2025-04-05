525b3e2eeba795be7643559ecaaff096d58231ab
RobinTT <156633350+RobinTT69@users.noreply.github.com>
2025-04-04 21:51:46 -0400

    Update listeners.rs

--- src-tauri/src/listeners.rs
+++ src-tauri/src/listeners.rs
@@ -1,22 +1,18 @@
 use notify::event::{ModifyKind, RenameMode};
 use notify::{Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
+use std::fs;
 
 use crate::utils::get_screenshot_dir;
 use crate::{files, upload};
 
 pub fn watch_file_system(app: tauri::AppHandle) {
     let mut last_image = String::new();
-
     let path = get_screenshot_dir();
-
     let (tx, rx) = channel();
 
     let mut watcher = RecommendedWatcher::new(tx, Config::default()).unwrap();
-
-    watcher
-        .watch(path.as_ref(), RecursiveMode::NonRecursive)
-        .unwrap();
+    watcher.watch(path.as_ref(), RecursiveMode::NonRecursive).unwrap();
 
     loop {
         for res in &rx {
@@ -27,10 +23,34 @@ pub fn watch_file_system(app: tauri::AppHandle) {
                         && last_image != event.paths[0].to_str().unwrap()
                         && files::is_image(&event.paths[0])
                     {
-                        last_image = event.paths[0].to_str().unwrap().to_owned();
-                        #[cfg(target_os = "linux")]
-                        files::copy_image_to_clipboard(event.paths[0].as_path());
-                        upload::upload_file_to_host(event.paths[0].as_path(), &app);
+                        let file_path = event.paths[0].as_path();
+                        
+                        // Validate plsssssssssssssssssss.
+                        match fs::metadata(file_path) {
+                            Ok(metadata) => {
+                                if metadata.len() == 0 {
+                                    println!(
+                                        "File {} is empty. Skipping upload.",
+                                        file_path.display()
+                                    );
+                                    continue;
+                                }
+                            }
+                            Err(e) => {
+                                println!(
+                                    "Error reading metadata for {}: {}. Skipping upload.",
+                                    file_path.display(),
+                                    e
+                                );
+                                continue;
+                            }
+                        }
+                        
+                        last_image = file_path.to_str().unwrap().to_owned();
+         
+                        files::copy_image_to_clipboard(file_path);
+                        
+                        upload::upload_file_to_host(file_path, &app);
                     }
                 }
                 Err(error) => {
